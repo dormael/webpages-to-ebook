a7987937a67045c98c6ded1307d9e6c0d634a9cf
Goohyun Chung <goohyun.chung@navercorp.com>
2022-01-04 10:07:33 +0900

    extract content list from nav.html

---
+++ extract.nav.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep href nav.html |grep -v 'href="#'|grep -v 'href="."'|grep -v 'href="https'|trim.sh|cut -f 2 -d ' '|sed 's/href="//' | sed 's/"$//' | while read -r l; do echo "- https://argoproj.github.io/argo-rollouts/${l}"; done
